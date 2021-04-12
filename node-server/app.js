10915c5b45d7e50c7c1a8eaaf7b9767b59d8b295
naoki hirata <naoki@magic3.org>
2021-04-13 08:32:19 +0900

    初回登録

---
+++ node-server/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const path = require('path')
+
+const HOSTNAME = '127.0.0.1'
+const PORT = 3000
+const app = express()
+const server = require('http').createServer(app)
+
+// ### ルーティングの設定 ###
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const router = express.Router()
+
+router.get('/', function (request, response) {
+  response.render('index', { title: 'Hello!' })
+})
+
+router.get('/student', function (request, response) {
+  response.render('index', { title: 'Hello, student!' })
+})
+
+router.get('/teacher', function (request, response) {
+  response.render('index', { title: 'Hello, teacher!' })
+})
+
+app.use('/', router)
+
+// ### Socket接続処理 ###
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+  console.log('connect by socket')
+
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
+
+// ### サーバ起動 ###
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`)
+})
