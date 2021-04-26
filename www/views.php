cd6d3d612eb46539df871f919931aa1dad8d1d8f
naoki hirata <naoki@magic3.org>
2021-04-26 15:42:06 +0900

    PHPフロントエンド更新。

--- www/views.php
+++ www/views.php
@@ -1,10 +1,8 @@
 <?php
-
 require_once('./lib/ExpressSessionHandler.php');
-	  
 use danfsd\ExpressSessionHandler;
-	  
-	  // This is the express-session's secret you defined in your NodeJS application
+      
+// This is the express-session's secret you defined in your NodeJS application
 const SESSION_SECRET = "node.js rules";
 
 $handler = new ExpressSessionHandler(SESSION_SECRET);
@@ -18,12 +16,39 @@ session_start();
 // Populates $_SESSION['cookie'] with data that express-session requires
 $handler->populateSession();
 
-if (isset($_SESSION['views'])){
-	$_SESSION['views']++;
+if (isset($_SESSION['views'])) {
+    $_SESSION['views']++;
 } else {
-	$_SESSION['views'] = 1;
+    $_SESSION['views'] = 1;
 }
-echo 'アクセス数: ' . $_SESSION['views'];
-//var_dump($_SESSION);
-//var_dump($_COOKIE);
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <title>PHPフロントエンド</title>
+</head>
+
+<body>
+  <div class="d-flex flex-column min-vh-100 align-items-center py-5">
+    <div class="card">
+      <div class="card-header">
+        <h4>PHPフロントエンド</h4>
+      </div>
+      <div class="card-body">
+        <h5>アクセス数：<?php echo $_SESSION['views'];?>
+        </h5>
+        <input type="button" class="btn btn-primary" value="更新" onclick="window.location.reload();">
+      </div>
+    </div>
+  </div>
+  </div>
+</body>
+
+</html>
